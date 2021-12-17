2683c9a85102a45cddd3b4b92a29843bbecd2d34
Vytenis Narušis <vytenis@billo.app>
2021-12-17 11:42:12 +0200

    AoC 2021 Complete day 12

--- AdventOfCode/2021/12/12.py
+++ AdventOfCode/2021/12/12.py
@@ -19,15 +19,28 @@ class Cave:
     name: str
     adjacent: list
     size: CaveSize
+    times_visited: int
 
     def __init__(self, name: str):
         self.name = name
         self.adjacent = []
         self.size = CaveSize.SMALL if name.islower() else CaveSize.LARGE
+        match self.size:
+            case CaveSize.SMALL:
+                self.visits_left = 1
+            case CaveSize.LARGE:
+                self.visits_left = None
+        self.times_visited = 0
 
     def append_adjacent(self, cave):
         if not cave in self.adjacent:
             self.adjacent.append(cave)
+
+    def __str__(self):
+        return f'Cave({self.name})'
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class CaveMap:
@@ -38,44 +51,100 @@ class CaveMap:
 
     @staticmethod
     def from_lines(lines):
-        map = CaveMap()
+        cave_map = CaveMap()
         for line in lines:
             [name_a, name_b] = line.strip().split('-')
-            cave_a = map.cave_by_name.get(name_a)
+            cave_a = cave_map.cave_by_name.get(name_a)
             if not cave_a:
                 cave_a = Cave(name_a)
-                map.cave_by_name[name_a] = cave_a
+                cave_map.cave_by_name[name_a] = cave_a
 
-            cave_b = map.cave_by_name.get(name_b)
+            cave_b = cave_map.cave_by_name.get(name_b)
             if not cave_b:
                 cave_b = Cave(name_b)
-                map.cave_by_name[name_b] = cave_b
+                cave_map.cave_by_name[name_b] = cave_b
 
             cave_a.append_adjacent(cave_b)
             cave_b.append_adjacent(cave_a)
 
-        return map
+        return cave_map
 
 
-def walk(cave: Cave, stack):
-    for adj in cave.adjacent:
-        if adj.name == 'end':
-            print(stack)
+def walk(cave: Cave, stack: list[Cave], state):
+    stack.append(cave)
+    cave.times_visited += 1
 
-        if adj not in stack:
-            walk()
+    if cave.name == 'end':
+        state['ends'] += 1
+    else:
+        for adj in cave.adjacent:
+            if adj.size == CaveSize.LARGE or adj.times_visited == 0:
+                walk(adj, stack, state)
+
+    stack.pop()
+    cave.times_visited -= 1
+
+
+def should_visit_adjacent_two(adj, state):
+    if adj.name == 'start':
+        return False
+
+    if adj.name == 'end':
+        return True
+
+    match adj.size:
+        case CaveSize.SMALL:
+            if state['visited_small_cave_twice']:
+                return adj.times_visited < 1
+            else:
+                return adj.times_visited < 2
+        case CaveSize.LARGE:
+            return True
+
+
+def walk_two(cave: Cave, stack: list[Cave], state):
+    stack.append(cave)
+    cave.times_visited += 1
+
+    visited_this_twice = cave.size == CaveSize.SMALL and cave.times_visited == 2
+    if visited_this_twice:
+        state['visited_small_cave_twice'] = True
+
+    if cave.name == 'end':
+        state['ends'] += 1
+    else:
+        for adj in cave.adjacent:
+            if should_visit_adjacent_two(adj, state):
+                walk_two(adj, stack, state)
+
+    if visited_this_twice:
+        state['visited_small_cave_twice'] = False
+    stack.pop()
+    cave.times_visited -= 1
 
 
 def part_one(in_file_name):
     with open(in_file_name, 'r', encoding='utf-8') as in_file:
         cave_map = CaveMap.from_lines(in_file.readlines())
+        state = {'ends': 0}
 
-        walk(cave_map.cave_by_name['start'], [])
+        walk(cave_map.cave_by_name['start'], [], state)
+
+        print(state['ends'])
 
 
 def part_two(in_file_name):
-    with open(in_file_name, 'r') as in_file:
-        pass
+    with open(in_file_name, 'r', encoding='utf-8') as in_file:
+        cave_map = CaveMap.from_lines(in_file.readlines())
+
+        state = {
+            'ends': 0,
+            'visited_small_cave_twice': False
+        }
+
+        walk_two(cave_map.cave_by_name['start'], [], state)
+
+        print(state['ends'])
 
 
 if __name__ == '__main__':
