98b404b0778942f6340521e98d977219d13b345c
Vytenis Narušis <vytenis.narusis@gmail.com>
2021-12-20 07:36:24 +0200

    AoC 2021 Progress on day 17

--- AdventOfCode/2021/17/17.py
+++ AdventOfCode/2021/17/17.py
@@ -1,3 +1,4 @@
+from io import FileIO
 from os import stat
 import sys
 import argparse
@@ -12,12 +13,26 @@ sys.path.append('../../../')
 from util.term_control import TermControl, TermColor  # pylint: disable=wrong-import-position,import-error
 
 
-def part_one(in_file, out_file):
+def part_one(in_file: FileIO, out_file: FileIO):
+    target_spec = in_file.readline()
+    target_spec = target_spec.strip().lstrip('target area: ')
+    coords = [c.split('=') for c in target_spec.split(', ')]
+    for coord in coords:
+        coord[1] = coord[1].split('..')
+
+    x_min = coords[0][1][0]
+    x_max = coords[0][1][1]
+    y_min = coords[1][1][0]
+    y_max = coords[1][1][1]
+
+    vxi = 1
+    passed_x = False
+    while not passed_x:
 
 
-def main(in_file, part):
-    with open(f'{in_file}.in', 'r', encoding='utf-8') as in_file:
-        with open(f'{in_file}.{part}.out', 'w', encoding='utf-8') as out_file:
+def main(file_name, part):
+    with open(f'{file_name}.in', 'r', encoding='utf-8') as in_file:
+        with open(f'{file_name}.{part}.out', 'w', encoding='utf-8') as out_file:
             match part:
                 case 'one':
                     part_one(in_file, out_file)
