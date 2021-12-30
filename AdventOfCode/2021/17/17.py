f95c1daedcae055a8407cd8d11f505e7547d5eeb
Vytenis Narušis <vytenis@billo.app>
2021-12-30 22:55:03 +0200

    AoC 2021 Complete day 17

--- AdventOfCode/2021/17/17.py
+++ AdventOfCode/2021/17/17.py
@@ -13,6 +13,47 @@ sys.path.append('../../../')
 from util.term_control import TermControl, TermColor  # pylint: disable=wrong-import-position,import-error
 
 
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
+class Area:
+    x_min: int
+    x_max: int
+    y_min: int
+    y_max: int
+
+    def has_point(self, point: Point):
+        return point.x >= self.x_min and \
+            point.x <= self.x_max and \
+            point.y >= self.y_min and \
+            point.y <= self.y_max
+
+    def has_passed(self, point: Point):
+        return point.x > self.x_max or \
+            point.y < self.y_min
+
+
+def check_if_hits_target(vxi: int, vyi: int, area: Area):
+    vx = vxi
+    vy = vyi
+    position = Point(0, 0)
+
+    while not area.has_passed(position):
+        if area.has_point(position):
+            return True
+
+        position.x += vx
+        position.y += vy
+        vx = max(vx - 1, 0)
+        vy = vy - 1
+
+    return False
+
+
 def part_one(in_file: FileIO, out_file: FileIO):
     target_spec = in_file.readline()
     target_spec = target_spec.strip().lstrip('target area: ')
@@ -25,41 +66,47 @@ def part_one(in_file: FileIO, out_file: FileIO):
     y_min = int(coords[1][1][0])
     y_max = int(coords[1][1][1])
 
-    print(x_min, x_max, y_min, y_max)
+    area = Area(x_min, x_max, y_min, y_max)
 
-    highest_vyi = 0
+    vy = 77
+    y = 0
 
-    vyi = 0
-    highest_y = 0
-    done = False
-    while not done:
-        vy = vyi
-        y = 0
-        py = 0
+    while True:
+        print('y', y)
+        print('vy', vy)
 
-        while y > y_min:
-            y += vy
-            if y > highest_y:
-                highest_y = y
-            vy -= 1
+        input()
 
-            if y < y_min:
-                if py > y_max:
-                    pass
-                else:
-                    highest_vyi = vyi
-                    vyi += 1
-                    print('New high', highest_vyi)
-                    print('Highest y', highest_y)
-
-                break
-
-            py = y
-
-        vyi += 1
-        highest_y = 0
+        y += vy
+        vy -= 1
 
     print(highest_vyi)
+    print(highest_y)
+
+
+def part_two(in_file: FileIO, out_file: FileIO):
+    target_spec = in_file.readline()
+    target_spec = target_spec.strip().lstrip('target area: ')
+    coords = [c.split('=') for c in target_spec.split(', ')]
+    for coord in coords:
+        coord[1] = coord[1].split('..')
+
+    x_min = int(coords[0][1][0])
+    x_max = int(coords[0][1][1])
+    y_min = int(coords[1][1][0])
+    y_max = int(coords[1][1][1])
+
+    area = Area(x_min, x_max, y_min, y_max)
+    print(area)
+
+    count = 0
+    for vxi in range(0, x_max + 1):
+        for vyi in range(y_min, -y_min + 1):
+            if check_if_hits_target(vxi, vyi, area):
+                print(vxi, ',', vyi)
+                count += 1
+
+    print('Total:', count)
 
 
 def main(file_name, part):
